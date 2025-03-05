0cd59aea44f2465268f21b859546ddaf6276649b
Jaydeep Gohil <167198695+Jaydeep-Gohil@users.noreply.github.com>
2025-03-05 19:31:17 +0530

    Add new class files and resources for Assignment 2 in Advanced Java

---
+++ Semester-4/Advance Java/Assignment-2/Assignment_2/src/assignment_2/ResultsSet.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment_2;
+
+/**
+ *
+ * @author LENOV
+ */
+class ResultsSet {
+
+    boolean first() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    String getString(String empnO) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
