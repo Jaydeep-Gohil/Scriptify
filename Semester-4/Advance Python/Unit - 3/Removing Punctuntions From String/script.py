3dc6633c5c7cbaa60d0a8756ea5dd81b414ca29c
Jaydeep Gohil <167198695+Jaydeep-Gohil@users.noreply.github.com>
2025-03-08 21:13:36 +0530

    Add script to remove punctuation from a string

---
+++ Semester-4/Advance Python/Unit - 3/Removing Punctuntions From String/script.py
@@ -0,0 +1,8 @@
+
+text = "Hello! This is a sample text, with some punctuation marks. How are you?"
+punc = "."
+
+for char in text:
+    if char in punc:
+        text = text.replace(char, "")
+print(text)
