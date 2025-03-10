415ee41f5aacec662547753e3f47a5d9f866947d
Jaydeep Gohil <167198695+Jaydeep-Gohil@users.noreply.github.com>
2025-03-10 18:53:05 +0530

    Add PHP script to demonstrate the use of increment and decrement operators

---
+++ Semester-4/Web Programming - 2/Unit - 1/Increment & Decrement Operator/script.php
@@ -0,0 +1,20 @@
+
+<html>
+
+<body bgcolor="pink">
+    <?php
+    $a = 10;
+    $b = 20;
+
+    echo $a++;
+    echo "<br>";
+    echo ++$a;
+    echo "<br>";
+    echo $b++;
+    echo "<br>";
+    echo --$b;
+    echo "<br>";
+    ?>
+</body>
+
+</html>
