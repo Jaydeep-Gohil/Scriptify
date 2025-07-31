335ae27cf9b71019582f4a8d77e879bf53d56303
Jaydeep Gohil <167198695+Jaydeep-Gohil@users.noreply.github.com>
2025-07-31 09:04:51 +0530

    Add initial implementation of SwapeNumber project
    - Created solution file and project structure for SwapeNumber.
    - Implemented number swapping logic in Program.cs.
    - Added necessary project files including .csproj and various metadata files.
    - Generated build artifacts for Debug configuration targeting .NET 8.0.
    - Included Visual Studio specific files for project management and settings.

---
+++ Semester-5/C#/Leb Exercise 4/SwapeNumber/SwapeNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Programe {
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Firts Number :");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter Second Number :");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int temp;
+
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        Console.WriteLine("After Swape");
+        Console.WriteLine("First Numebr :"+num1);
+        Console.WriteLine("Second Numebr :"+num2);
+    }
+}
